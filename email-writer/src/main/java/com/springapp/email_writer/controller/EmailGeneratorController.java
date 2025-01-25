d9e4a728a7e18d60ef4b159c100c6414cb1fd33c
Anushka <anushkamar1102@gmail.com>
2025-01-25 14:51:52 +0530

    created the RestController requests a PostMapping

---
+++ email-writer/src/main/java/com/springapp/email_writer/controller/EmailGeneratorController.java
@@ -0,0 +1,27 @@
+package com.springapp.email_writer.controller;
+
+import com.springapp.email_writer.Service.EmailGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @Autowired
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
